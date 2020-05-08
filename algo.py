f600cc2eace76b58ba675a8c67e2ebe3fc02fd48
Brouney <piotreg98@gmail.com>
2020-05-08 18:50:31 +0200

    pre implementation of algo

---
+++ algo.py
@@ -0,0 +1,66 @@
+import numpy as np
+import json
+
+
+class Alg:
+
+    def generate_element(self,forexname,to,day,minute,price):
+        element = {
+            "forex":forexname,
+            "to": to,
+            "day": day,
+            "minute": minute,
+            "price":price
+        }
+        return element
+
+    def addToCurrencies(self,curr,toadd):
+        curr["currencies"].append(toadd)
+
+    def addElementToCurrency(self,currencies,currency,elem):
+        
+        for tmp in currencies["currencies"]:
+            if tmp == currency :
+                tmp["elements"].append(elem)
+
+    def generate_data(self):
+        currencies = {
+            "currencies": []
+        }
+        BTC = {
+            "elements": []
+        }
+        
+        ETH = {
+            "elements": []
+        }
+        
+        RIP = {
+            "elements": []
+        }
+        self.addToCurrencies(currencies,BTC)
+        self.addToCurrencies(currencies,ETH)
+        self.addToCurrencies(currencies,RIP)
+
+        tmp_price = np.random.rand(100)
+
+        for i in range(10):
+            elem = self.generate_element('a','1','USD',str(i),tmp_price[i])
+            self.addElementToCurrency(currencies,BTC,elem)
+            self.addElementToCurrency(currencies,ETH,elem)
+            self.addElementToCurrency(currencies,RIP,elem)
+            elem = self.generate_element('b','1','RIP',str(i),tmp_price[i+10])
+            self.addElementToCurrency(currencies,BTC,elem)
+            self.addElementToCurrency(currencies,ETH,elem)
+            #self.addElementToCurrency(currencies,RIP,elem)
+            elem = self.generate_element('c','1','BTC',str(i),tmp_price[i+10])
+            #self.addElementToCurrency(currencies,BTC,elem)
+            self.addElementToCurrency(currencies,ETH,elem)
+            self.addElementToCurrency(currencies,RIP,elem)
+
+
+        print(json.dumps(currencies,indent=4))
+
+if __name__ == "__main__":
+    algo = Alg()
+    algo.generate_data()
--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 
 import json
-
+import pandas as pd
+import datetime
 #pip install pycoingecko https://github.com/man-c/pycoingecko
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
 
-print(json.dumps(cg.get_indexes(),indent=4))
+#print(json.dumps(cg.get_indexes(),indent=4))
 
-print(cg.get_price(ids='bitcoin,litecoin,ethereum', vs_currencies='BTC'))
+#print(cg.get_price(ids='bitcoin,litecoin,ethereum', vs_currencies='BTC'))
+#print(cg.get_coin_market_chart_by_id(id='bitcoin',vs_currency='usd',days='2'))
+print(cg.get_coin_history_by_id(id='bitcoin',date='13-12-2019'))
 
 #pip install alphavantage https://pypi.org/project/alphavantage/
 
-from alphavantage.price_history import (
-  AdjustedPriceHistory, get_results, PriceHistory, IntradayPriceHistory,
-  filter_dividends
-)
+# from alphavantage.price_history import (
+#   AdjustedPriceHistory, get_results, PriceHistory, IntradayPriceHistory,
+#   filter_dividends
+# )
 
-parameters = {'output_size': 'compact', 'period': 'W'}
-tickers = ['AAPL', 'MSFT']
-results = dict(get_results(PriceHistory, tickers, parameters))
+# parameters = {'output_size': 'compact', 'period': 'W'}
+# tickers = ['AAPL', 'MSFT']
+# results = dict(get_results(PriceHistory, tickers, parameters))
 #returns nothing
-print(results)
+#print(results)
