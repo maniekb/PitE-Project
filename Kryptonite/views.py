3c8bb65b09292f33d9d9fea47f1a18ab107d11e0
Maciej Dworak <dworak.maciej@gmail.com>
2020-05-09 23:09:08 +0200

    Added basic page with working register, login and logout

--- Kryptonite/views.py
+++ Kryptonite/views.py
@@ -1,8 +1,52 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from .models.forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+import logging
 
-# Create your views here.
-from django.http import HttpResponse
+logger = logging.getLogger(__name__)
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                return render(request, 'register.html', {'form': form})
+
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                return render(request, 'login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
