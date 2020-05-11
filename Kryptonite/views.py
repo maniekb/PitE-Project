8660a1a54ba69c528f84077994fb64f9a11d2015
Piotr Ksel <piotrek_ksel@wp.pl>
2020-05-12 01:18:50 +0200

    POC using plotly to create charts with django

--- Kryptonite/views.py
+++ Kryptonite/views.py
@@ -2,13 +2,21 @@ from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
 from .models.forms import RegistrationForm
 from django.contrib.auth.forms import AuthenticationForm
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 def index(request):
-    return render(request, 'index.html')
+    x_data = [0, 1, 2, 3]
+    y_data = [x ** 2 for x in x_data]
+    plot_div = plot([Scatter(x=x_data, y=y_data,
+                             mode='lines', name='test',
+                             opacity=0.8, marker_color='green')],
+                    output_type='div')
+    return render(request, 'index.html', context={'plot_div': plot_div})
 
 
 def register(request):
