7a059340b58d12cfecbb899088b3f0f6d259b962
Maciej Dworak <dworak.maciej@gmail.com>
2020-05-12 02:00:48 +0200

    Added initial models for storing user config

--- Kryptonite/models/models.py
+++ Kryptonite/models/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-# Create your models here.
+class Currency(models.Model):
+    label = models.CharField(max_length=30)
+    value = models.CharField(max_length=30)
+
+
+class Exchange(models.Model):
+    label = models.CharField(max_length=30)
+    value = models.CharField(max_length=30)
+
+
+class FavouriteCurrency(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+
+class FavouriteExchange(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exchange = models.ForeignKey(Exchange, on_delete=models.CASCADE)
