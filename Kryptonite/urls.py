5e381dac7199faf643ed3b19b819ed33fb3c3857
Maciej Dworak <dworak.maciej@gmail.com>
2020-05-12 02:02:05 +0200

    Added favourites page with initial functionality of adding and removing user favourites currencies and exchanges

--- Kryptonite/urls.py
+++ Kryptonite/urls.py
@@ -6,5 +6,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
     path('login', views.login_user, name='login'),
-    path('logout', views.logout_user, name='logout')
+    path('logout', views.logout_user, name='logout'),
+    path('arbitrage', views.arbitrage, name='arbitrage'),
+    path('account', views.account, name='account'),
+    path('favourites', views.favourites, name='favourites'),
+    path('del_favourite_currency', views.del_favourite_currency, name='del_favourite_currency'),
+    path('add_favourite_currency', views.add_favourite_currency, name='add_favourite_currency'),
+    path('add_favourite_exchange', views.add_favourite_exchange, name='add_favourite_exchange'),
+    path('del_favourite_exchange', views.del_favourite_exchange, name='del_favourite_exchange')
 ]
--- Kryptonite/views.py
+++ Kryptonite/views.py
@@ -1,14 +1,22 @@
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
 from .models.forms import RegistrationForm
+from .models.models import Currency, FavouriteCurrency, FavouriteExchange, Exchange
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.http import Http404
+
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 def index(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        return render(request, 'user_index.html')
+    else:
+        return render(request, 'index.html')
 
 
 def register(request):
@@ -47,6 +55,82 @@ def login_user(request):
     return render(request, 'login.html', {'form': form})
 
 
+@login_required
 def logout_user(request):
     logout(request)
     return redirect('/')
+
+
+@login_required
+def favourites(request):
+    c_ids = [fav.currency_id for fav in FavouriteCurrency.objects.filter(user_id=request.user.id)]
+
+    all_currencies = Currency.objects.all()
+    fav_curr = [curr for curr in all_currencies if curr.id in c_ids]
+    rest_curr = [curr for curr in all_currencies if curr.id not in c_ids]
+
+    e_ids = [fav.exchange_id for fav in FavouriteExchange.objects.filter(user_id=request.user.id)]
+
+    all_exchanges = Exchange.objects.all()
+    fav_exchange = [ex for ex in all_exchanges if ex.id in e_ids]
+    rest_exchange = [ex for ex in all_exchanges if ex.id not in e_ids]
+    return render(request, 'favourites.html',
+                  {'currencies': rest_curr,
+                   'curr_buttons': fav_curr,
+                   "exchange_buttons": fav_exchange,
+                   "exchanges": rest_exchange})
+
+
+@login_required
+def del_favourite_currency(request):
+    if not request.is_ajax():
+        raise Http404
+    else:
+        curr_id = request.POST.get("id", None)
+        FavouriteCurrency.objects.filter(user_id=request.user.id, currency_id=curr_id).delete()
+        return JsonResponse({"id": curr_id})
+
+
+@login_required
+def add_favourite_currency(request):
+    if not request.is_ajax():
+        raise Http404
+    else:
+        curr_id = request.POST.get("id", None)
+        logger.warning(curr_id)
+        fav = FavouriteCurrency(user_id=request.user.id, currency_id=curr_id)
+        fav.save()
+        return JsonResponse({"id": curr_id})
+
+
+@login_required
+def del_favourite_exchange(request):
+    if not request.is_ajax():
+        raise Http404
+    else:
+        exch_id = request.POST.get("id", None)
+        FavouriteExchange.objects.filter(user_id=request.user.id, exchange_id=exch_id).delete()
+        return JsonResponse({"id": exch_id})
+
+
+@login_required
+def add_favourite_exchange(request):
+    if not request.is_ajax():
+        raise Http404
+    else:
+        exch_id = request.POST.get("id", None)
+        fav = FavouriteExchange(user_id=request.user.id, exchange_id=exch_id)
+        fav.save()
+        return JsonResponse({"id": exch_id})
+
+
+@login_required
+def account(request):
+    # TODO
+    return render(request, 'account.html')
+
+
+@login_required
+def arbitrage(request):
+    # TODO
+    return render(request, 'arbitrage.html')
