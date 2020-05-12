46babf2e98123bf0b61adeb908071aea083d6a25
Maciej Dworak <dworak.maciej@gmail.com>
2020-05-13 01:28:57 +0200

    Adjustments after merging chart code

--- Kryptonite/static/chart.js
+++ Kryptonite/static/chart.js
@@ -3,49 +3,47 @@ var endpoint = '/api/data/'
 var BinanceBTC = []
 var Labels = []
 
-$(document).ready(function() {
+$(document).ready(function () {
     $.ajax({
         method: "GET",
         url: endpoint,
-        success: function(data){
+        success: function (data) {
             console.log(data)
-            for(var i = 0; i < data.length; i++)
-            {
+            for (var i = 0; i < data.length; i++) {
                 BinanceBTC.push(Number(data[i].open));
                 date = new Date(data[i].open_time);
                 Labels.push(date.toShortFormat());
             }
             drawChart();
         },
-        error: function(error_data){
-        console.log(error_data)
+        error: function (error_data) {
+            console.log(error_data)
         }
     });
 });
 
 
-
-drawChart = function() {
+drawChart = function () {
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: Labels,
-            datasets:[{
+            datasets: [{
                 data: BinanceBTC,
                 label: "BTC - Binance",
                 fill: false,
                 borderColor: 'rgba(255, 0, 0, 0.6)',
                 hoverBorderColor: 'rgba(255, 0, 0, 0.8)'
             }]
-        },     
+        },
         options: {
             title: {
                 display: true,
                 text: 'Historical Data',
                 fontColor: 'rgba(0, 0, 0, 0.6)',
                 fontSize: 20
-              },
+            },
             legend: {
                 labels: {
                     fontColor: 'rgb(255, 99, 132)',
@@ -55,7 +53,7 @@ drawChart = function() {
             scales: {
                 xAxes: [{
                     ticks: {
-                        callback: function(tick, index, array) {
+                        callback: function (tick, index, array) {
                             return (index % 4) ? "" : tick;
                         }
                     }
@@ -63,18 +61,19 @@ drawChart = function() {
             }
         }
     });
+    $("#myChart").css('visibility', 'visible');
 }
 
-Date.prototype.toShortFormat = function() {
+Date.prototype.toShortFormat = function () {
 
-    var month_names =["Jan","Feb","Mar",
-                      "Apr","May","Jun",
-                      "Jul","Aug","Sep",
-                      "Oct","Nov","Dec"];
-    
+    var month_names = ["Jan", "Feb", "Mar",
+        "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep",
+        "Oct", "Nov", "Dec"];
+
     var day = this.getDate();
     var month_index = this.getMonth();
     var year = this.getFullYear();
-    
+
     return "" + day + " " + month_names[month_index] + " " + year;
 }
