acc1a0ce90f89c4669ac809986dc5da638ee613d
Brouney <piotreg98@gmail.com>
2020-05-20 13:09:31 +0200

    edited and added maxval

--- algo.py
+++ algo.py
@@ -2,123 +2,75 @@ import numpy as np
 import json
 
 
-class Alg:
-
-   
-
-    def addToCurrencies(self,curr,toadd):
-        curr["currencies"].append(toadd)
-
-    def addElementToCurrency(self,currencies,currency,elem):
-        
-        for tmp in currencies["currencies"]:
-            if tmp == currency :
-                tmp["elements"].append(elem)
-
-    def findPath(self,forexlist,data):
-        maxincome = 0
-        for tmp,f in enumerate(forexlist):
-            for i,forex in enumerate(forexlist):
-                if tmp == i:
-                    continue
-
-
-    def generate_element(self,forexname,to,day,minute,price):
-        element = {
-            "to": to,
-            "day": day,
-            "minute": minute,
-            "price":price
-        }
-        return element
-    def generate_data(self):
-        
-         
-        
-        forex = {
-            "forex":[]
-        }
-        currencies = {
-            "currencies": []
-        }
-        BTC = {
-            "elements": []
-        }
-        
-        ETH = {
-            "elements": []
-        }
-        
-        RIP = {
-            "elements": []
-        }
-        self.addToCurrencies(currencies,BTC)
-        self.addToCurrencies(currencies,ETH)
-        self.addToCurrencies(currencies,RIP)
-
-        tmp_price = [1,2,0.5]
-
-        for i in range(10):
-            elem = self.generate_element('a','1','USD',str(i),tmp_price[i])
-            self.addElementToCurrency(currencies,BTC,elem)
-            self.addElementToCurrency(currencies,ETH,elem)
-            self.addElementToCurrency(currencies,RIP,elem)
-            elem = self.generate_element('b','1','RIP',str(i),tmp_price[i+10])
-            self.addElementToCurrency(currencies,BTC,elem)
-            self.addElementToCurrency(currencies,ETH,elem)
-            #self.addElementToCurrency(currencies,RIP,elem)
-            elem = self.generate_element('c','1','BTC',str(i),tmp_price[i+10])
-            #self.addElementToCurrency(currencies,BTC,elem)
-            self.addElementToCurrency(currencies,ETH,elem)
-            self.addElementToCurrency(currencies,RIP,elem)
-
-
-        print(json.dumps(currencies,indent=4))
-if __name__ == "__main__":
-    algo = Alg()
-    algo.generate_data()
-
-
 data = {
     "name":[2,4,5,6]
 }
 print(data["name"][2])
 
+#generate combination of triangular
 def generateCombinationOfThree(tocombinate):
     tmp = [[0,1,2],[0,2,1]]
     return tmp
 
-def returnCurrenciesData(data,currency):
+#returns element from "data" for "currency"
+def returnSpecyficCurrencyFromData(data,currency):
     for currencies in data["data"]:
         if currencies["currency"] == currency:
             return currencies
 
-def returnCurrenciesDataFROMTO(data,ffrom,to):
-    currencies = returnCurrenciesData(data,ffrom)
+#returns element in "trades" when "currency" in "data" is ffrom and "change_to" is to
+def returnSpecyficCurrenciesInTrades(data,ffrom,to):
+    currencies = returnSpecyficCurrencyFromData(data,ffrom)
     for trades in currencies["trades"]:
         if trades["change_to"] == to:
             return trades
 
-def algorithm(forex,start_currency,start_value):
+#returns element from specyfic element in "trades" is data and returns elements "rate" 
+def returnSpecyficRecord(data,record):
+    return data["records"][record]["rate"]
+
+#returns value after changing 
+def returnEndCount(list_rates,start_value):
+    val = start_value
+    val *= list_rates[0]
+    val /= list_rates[1]
+    val *= list_rates[2]
+    return val
+        
+
+#main algorithm
+def algorithm(forex,start_currency,start_value,records):
     tmp = forex["gielda"].length
     combination = generateCombinationOfThree(tmp)
 
     for i in combination:
-        valueon1 = start_value
+        valuestart = start_value
         max_value = 0
-
+        temporary_max = 0
         g1 = forex["gielda"][ i[0] ]
         g2 = forex["gielda"][ i[1] ]
         g3 = forex["gielda"][ i[2] ]
-        #data for start_currency in g1
-        currenciesg1 = returnCurrenciesData(g1,start_currency)
         
-        for change_to_ong1 in currenciesg1["trades"]:
-            currenciesg2 = returnCurrenciesData(g2,change_to_ong1["currency"])
+        currenciesg1 = returnSpecyficCurrencyFromData(g1,start_currency) 
+        
+        for ong1,change_to_ong1 in enumerate(currenciesg1["trades"]):
+            currenciesg2 = returnSpecyficCurrencyFromData(g2,change_to_ong1["change_to"]) 
             
-            for change_to_ong2 in currenciesg2["trades"]:
-                if change_to_ong2["currency"] == start_value:
+            for ong2,change_to_ong2 in enumerate(currenciesg2["trades"]):
+                if change_to_ong2["change_to"] == start_currency :
                     continue
-                change_to_ong3 = returnCurrenciesDataFROMTO(g3,change_to_ong2["currency"],start_currency)
+                change_to_ong3 = returnSpecyficCurrenciesInTrades(g3,change_to_ong2["change_to"],start_currency)
+                
+                for rec in range(records):
+                    list_rates = []
+                    list_rates.append(returnSpecyficRecord(change_to_ong1,rec))
+                    list_rates.append(returnSpecyficRecord(change_to_ong2,rec))
+                    list_rates.append(returnSpecyficRecord(change_to_ong3,rec))
+
+                    temporary_max = returnEndCount(list_rates,start_value)
+                    max_value = max(temporary_max,max_value)
+
+                    
+
 
                 
