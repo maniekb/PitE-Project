b3830a800ac9143f6607e9650af34602d41186f3
Maciej Dworak <39861075+Aureliusz13@users.noreply.github.com>
2020-06-18 08:50:17 +0200

    Historic results (#42)
    * Added model for historic results and scheduler for updating last date result for BTC
    
    * Added chart with last week arbitrage opportunities

--- kryptonite/apps.py
+++ kryptonite/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class KryptoniteConfig(AppConfig):
     name = 'kryptonite'
-    #
-    # def ready(self):
-    #     from kryptonite.scheduler import logupdater
-    #     logupdater.start()
+
+    def ready(self):
+        from kryptonite.scheduler import logupdater
+        logupdater.start()
